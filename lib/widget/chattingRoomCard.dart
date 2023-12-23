eed9d5feac8695632b14fbf1a7d15bc3d3137880
ssd72 <sungdae.yi@gmail.com>
2023-12-23 18:44:03 +0100

    12월 23일 클론 코딩 카카오톡 - chat 표시 구역 구현중

--- lib/widget/chattingRoomCard.dart
+++ lib/widget/chattingRoomCard.dart
@@ -16,21 +16,74 @@ class ChattingRoomCard extends StatelessWidget {
       child: Stack(
         children: [
           Container(
-            height: 70,
+            height: 80,
             width: 400,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                ProfilImage(chat: chat),
                 Container(
-                  child: Column(
-                    children: [
-                      Text('11111'),
-                      Text('11111'),
-                    ],
+                  margin: EdgeInsets.only(left: 10, bottom: 3, right: 3),
+                  child: ProfilImage(chat: chat),
+                ),
+                Expanded(
+                  flex: 7,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 5, bottom: 20, right: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Text(
+                                chat.names.join(', '),
+                                overflow: TextOverflow.ellipsis,
+                                // 넘어갈 경우 "..."으로 표시
+                                maxLines: 1,
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontFamily: 'Exo2-Bold'),
+                              ),
+                              const SizedBox(
+                                width: 7,
+                              ),
+                              Text(
+                                '${chat.names.length}',
+                                maxLines: 1,
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.teal,
+                                    fontFamily: 'Exo2-Bold'),
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              chat.updateChat,
+                              overflow: TextOverflow.ellipsis,
+                              // 넘어갈 경우 "..."으로 표시
+                              maxLines: 1,
+                              style: const TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.indigo,
+                                  fontFamily: 'Exo2-Bold'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                Container()
+                Expanded(
+                    flex: 2,
+                    child: Container(
+                        margin: EdgeInsets.only(left: 5, bottom: 10, right: 10),
+                      child: Text(chat.updateDate),
+                    ))
               ],
             ),
           ),
